1f1fb8fac131cce865f8e54ea6754ffb5c4cc59e
Ushal <ushalstha93@gmail.com>
2022-10-13 19:56:46 +0545

    edit category window popup

--- client/src/components/Pages/Category/EditCategory.js
+++ client/src/components/Pages/Category/EditCategory.js
@@ -50,24 +50,24 @@ const EditCategory = (props) => {
                 </Modal.Header>
                 <Modal.Body>
                     <Formik
-                        initialValues={null}
+                        initialValues={{ catID: "", catName: "", catStatus: "" }}
                         validationSchema={validateUpdateCategorySchema}
                     >
                         {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
                             <Form onSubmit={handleSubmit}>
                                 <Form.Group as={Row} className="mb-3" controlId="categoryID">
-                                    <Form.Label column sm="1">ID:</Form.Label>
+                                    <Form.Label column sm="2">ID :</Form.Label>
                                     <Col sm="5">
-                                        <Form.Control type="text" name="catID" placeholder="Category ID" defaultValue={null} style={{ borderColor: touched.catID && errors.catID ? "red" : null }} disabled />
+                                        <Form.Control type="text" name="catID" placeholder="Category ID" defaultValue={values.catID} style={{ borderColor: touched.catID && errors.catID ? "red" : null }} disabled />
                                         {touched.catID && errors.catID ? (
                                             <Col className="error-message">{errors.catID}</Col>
                                         ) : null}
                                     </Col>
                                 </Form.Group>
                                 <Form.Group as={Row} className="mb-3" controlId="categoryName">
-                                    <Form.Label column sm="2">NAME:</Form.Label>
+                                    <Form.Label column sm="2">NAME :</Form.Label>
                                     <Col sm="8">
-                                        <Form.Control type="text" name="catName" placeholder="Enter Category Name" onChange={handleChange} defaultValue={null} style={{ borderColor: touched.catID && errors.catID ? "red" : null }} />
+                                        <Form.Control type="text" name="catName" placeholder="Enter Category Name" onChange={handleChange} defaultValue={values.catName} style={{ borderColor: touched.catID && errors.catID ? "red" : null }} />
                                         {touched.catName && errors.catName ? (
                                             <Col className="error-message">{errors.catName}</Col>
                                         ) : null}
@@ -77,7 +77,7 @@ const EditCategory = (props) => {
                                     <Form.Label column sm="2">STATUS:</Form.Label>
                                     <Col sm="5">
                                         <Form.Select id="selectCategory" name="catStatus" onChange={handleChange} style={{ borderColor: touched.catID && errors.catID ? "red" : null }} >
-                                            <option defaultValue={null}>Select Status</option>
+                                            <option defaultValue={values.catStatus}>Select Status</option>
                                             <option value="Active">Active</option>
                                             <option value="Inactive">Inactive</option>
                                         </Form.Select>
