397733f4d2f51adcee227355990bcca8fa122553
Ushal <ushalstha93@gmail.com>
2022-10-17 11:49:39 +0545

    user registration page added

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,34 @@ import NavBarLeft from './components/NavBarLeft/NavBarLeft';
 import Category from './components/Pages/Category/Category';
 import Dashboard from './components/Pages/Dashboard/Dashboard';
 import Products from './components/Pages/Products/Products';
-// import DefaultForm from './components/Form/DefaultForm';
+import Registration from './components/Pages/Users/Registration';
 import './wrapper.css';
 
 const App = () => {
 
   return (
 
-    // <DefaultForm />
-    <Router>
-      <NavBar />
-      <div className='wrapper'>
-        <div className='row'>
-          <div className="col-2 pr-0" style={{ minWidth: "200px" }}>
-            <NavBarLeft />
-          </div>
-          <div className="col-8 pl-0">
-            <Routes>
-              <Route exact path='/' element={<Dashboard />} />
-              <Route path='/category' element={<Category />} />
-              <Route path='/products' element={<Products />} />
-              {/* <Route path='/customers' element={} />
-              <Route path='/orders' element={} /> */}
-            </Routes>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </Router>
+    <Registration />
+    // <Router>
+    //   <NavBar />
+    //   <div className='wrapper'>
+    //     <div className='row'>
+    //       <div className="col-2 pr-0" style={{ minWidth: "200px" }}>
+    //         <NavBarLeft />
+    //       </div>
+    //       <div className="col-8 pl-0">
+    //         <Routes>
+    //           <Route exact path='/' element={<Dashboard />} />
+    //           <Route path='/category' element={<Category />} />
+    //           <Route path='/products' element={<Products />} />
+    //           {/* <Route path='/customers' element={} />
+    //           <Route path='/orders' element={} /> */}
+    //         </Routes>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Footer />
+    // </Router>
 
   )
 }
---
+++ client/src/components/Pages/Users/Registration.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { Form, Col, Row, Button } from 'react-bootstrap'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import '../../../wrapper.css'
+
+const Registration = () => {
+
+    const validateFormSchema = Yup.object().shape({
+        username: Yup.string()
+            .required("* UserName is Required!")
+            .min(5, "* Must be Greater 5 characters!")
+            .max(15, "* Must be Less than 15 characters!"),
+        fullname: Yup.string()
+            .required("* Full Name is Required!")
+            .min(5, "* Must be Greater than 5 Character!")
+            .max(25, "* Must be Less than 25 characters!"),
+        address: Yup.string()
+            .required("* Address is Required!"),
+        contactno: Yup.number()
+            .required("* Contact Number is Required!")
+            .min(10, "* Must be of 10 numbers")
+            .positive("* Must be positive number!")
+            .integer("* Must be integer Value!"),
+        password: Yup.string()
+            .required("* Must be Required!")
+    })
+
+    return (
+
+        <div className="container">
+
+            <h2>Registration</h2>
+            <Formik
+                initialValues={{ username: "", fullname: "", address: "", contactno: "", password: "" }}
+                validationSchema={validateFormSchema}
+                onSubmit={(values, { resetForm }) => {
+
+                }}
+            >
+                {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group as={Row} className="mb-3" controlId="username">
+                            <Form.Label column sm="2">UserName :</Form.Label>
+                            <Col sm="5">
+                                <Form.Control type="text" name="username" placeholder="UserName" onChange={handleChange} value={values.username} style={{ borderColor: touched.username && errors.username ? "red" : null }} />
+                                {touched.username && errors.username ? (
+                                    <Col className="error-message">{errors.username}</Col>
+                                ) : null}
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="fullname">
+                            <Form.Label column sm="2">Full Name :</Form.Label>
+                            <Col sm="5">
+                                <Form.Control type="text" name="fullname" placeholder="Enter Full Name" onChange={handleChange} value={values.fullname} style={{ borderColor: touched.fullname && errors.fullname ? "red" : null }} />
+                                {touched.fullname && errors.fullname ? (
+                                    <Col className="error-message">{errors.fullname}</Col>
+                                ) : null}
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="address">
+                            <Form.Label column sm="2">Address :</Form.Label>
+                            <Col sm="5">
+                                <Form.Control type="text" name="address" placeholder="Enter Address" onChange={handleChange} value={values.address} style={{ borderColor: touched.address && errors.address ? "red" : null }} />
+                                {touched.address && errors.address ? (
+                                    <Col className="error-message">{errors.address}</Col>
+                                ) : null}
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="contactno">
+                            <Form.Label column sm="2">Contact No :</Form.Label>
+                            <Col sm="5">
+                                <Form.Control type="text" name="contactno" placeholder="Enter Contact Number" onChange={handleChange} value={values.contactno} style={{ borderColor: touched.contactno && errors.contactno ? "red" : null }} />
+                                {touched.contactno && errors.contactno ? (
+                                    <Col className="error-message">{errors.contactno}</Col>
+                                ) : null}
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="password">
+                            <Form.Label column sm="2">Password :</Form.Label>
+                            <Col sm="5">
+                                <Form.Control type="password" name="password" placeholder="Enter Password" onChange={handleChange} value={values.password} style={{ borderColor: touched.password && errors.password ? "red" : null }} />
+                                {touched.password && errors.password ? (
+                                    <Col className="error-message">{errors.password}</Col>
+                                ) : null}
+                            </Col>
+                        </Form.Group>
+                        <Button variant="outline-primary" size="sm" type="submit">Register</Button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+
+    )
+}
+
+export default Registration
