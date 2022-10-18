4b97fe294e942a5bf31460289fe91ec4084b037b
Ushal <ushalstha93@gmail.com>
2022-10-18 09:10:30 +0545

    register and login page updated

--- client/src/components/Form/Registration.js
+++ client/src/components/Form/Registration.js
@@ -35,77 +35,80 @@ const Registration = () => {
 
         <div className="registration-body">
             <div className="form-body p-4 shadow rounded">
-            <h1 className="text-center" style={{ position: "relative", top: "10px" }}>SIGN UP NOW</h1>
-            <Formik
-                initialValues={{ username: "", fullname: "", address: "", contactno: "", email: "", password: "" }}
-                validationSchema={validateRegisterSchema}
-                onSubmit={(values, { resetForm }) => {
-                    console.log(values)
-                }}
-            >
-                {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
-                    <Form onSubmit={handleSubmit} className="m-5">
-                        <Form.Group as={Row} className="mb-3" controlId="username">
-                            <Form.Label column sm="4">UserName:</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="text" name="username" placeholder="UserName" onChange={handleChange} value={values.username} style={{ borderColor: touched.username && errors.username ? "red" : null }} />
-                                {touched.username && errors.username ? (
-                                    <Col className="error-message">{errors.username}</Col>
-                                ) : null}
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="fullname">
-                            <Form.Label column sm="4">Full Name:</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="text" name="fullname" placeholder="Enter Full Name" onChange={handleChange} value={values.fullname} style={{ borderColor: touched.fullname && errors.fullname ? "red" : null }} />
-                                {touched.fullname && errors.fullname ? (
-                                    <Col className="error-message">{errors.fullname}</Col>
-                                ) : null}
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="address">
-                            <Form.Label column sm="4">Address:</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="text" name="address" placeholder="Enter Address" onChange={handleChange} value={values.address} style={{ borderColor: touched.address && errors.address ? "red" : null }} />
-                                {touched.address && errors.address ? (
-                                    <Col className="error-message">{errors.address}</Col>
-                                ) : null}
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="contactno">
-                            <Form.Label column sm="4">Contact No:</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="text" name="contactno" placeholder="Enter Contact Number" onChange={handleChange} value={values.contactno} style={{ borderColor: touched.contactno && errors.contactno ? "red" : null }} />
-                                {touched.contactno && errors.contactno ? (
-                                    <Col className="error-message">{errors.contactno}</Col>
-                                ) : null}
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="email">
-                            <Form.Label column sm="4">Email:</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="text" name="email" placeholder="Enter Email" onChange={handleChange} value={values.email} style={{ borderColor: touched.email && errors.email ? "red" : null }} />
-                                {touched.email && errors.email ? (
-                                    <Col className="error-message">{errors.email}</Col>
-                                ) : null}
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="password">
-                            <Form.Label column sm="4">Password:</Form.Label>
-                            <Col sm="8">
-                                <Form.Control type="password" name="password" placeholder="Enter Password" onChange={handleChange} value={values.password} style={{ borderColor: touched.password && errors.password ? "red" : null }} />
-                                {touched.password && errors.password ? (
-                                    <Col className="error-message">{errors.password}</Col>
-                                ) : null}
-                            </Col>
-                        </Form.Group>
-                        <Button variant="outline-primary" size="sm" type="submit" style={{ position: "relative", left: "8rem", marginTop: "10px" }}>SIGN UP</Button>
-                        <Form.Text className="text-muted" style={{ position: "relative", top: "2.5rem" }}>
-                            Already Have Account ? <Link to="/login">SIGN IN</Link>
-                        </Form.Text>
-                    </Form>
-                )}
-            </Formik>
+                <div className="text-center rounded shadow" style={{ background: "#1e363b", color: "white", marginBottom: "-20px" }}>
+                    <h1 className="" style={{ position: "relative" }}>SIGN UP NOW</h1>
+                    <span>Welcome To Inventory Management System</span>
+                </div>
+                <Formik
+                    initialValues={{ username: "", fullname: "", address: "", contactno: "", email: "", password: "" }}
+                    validationSchema={validateRegisterSchema}
+                    onSubmit={(values, { resetForm }) => {
+                        console.log(values)
+                    }}
+                >
+                    {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
+                        <Form onSubmit={handleSubmit} className="m-5">
+                            <Form.Group as={Row} className="mb-3" controlId="username">
+                                <Form.Label column sm="4">UserName:</Form.Label>
+                                <Col sm="8">
+                                    <Form.Control type="text" name="username" placeholder="UserName" onChange={handleChange} value={values.username} style={{ borderColor: touched.username && errors.username ? "red" : null }} />
+                                    {touched.username && errors.username ? (
+                                        <Col className="error-message">{errors.username}</Col>
+                                    ) : null}
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3" controlId="fullname">
+                                <Form.Label column sm="4">Full Name:</Form.Label>
+                                <Col sm="8">
+                                    <Form.Control type="text" name="fullname" placeholder="Enter Full Name" onChange={handleChange} value={values.fullname} style={{ borderColor: touched.fullname && errors.fullname ? "red" : null }} />
+                                    {touched.fullname && errors.fullname ? (
+                                        <Col className="error-message">{errors.fullname}</Col>
+                                    ) : null}
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3" controlId="address">
+                                <Form.Label column sm="4">Address:</Form.Label>
+                                <Col sm="8">
+                                    <Form.Control type="text" name="address" placeholder="Enter Address" onChange={handleChange} value={values.address} style={{ borderColor: touched.address && errors.address ? "red" : null }} />
+                                    {touched.address && errors.address ? (
+                                        <Col className="error-message">{errors.address}</Col>
+                                    ) : null}
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3" controlId="contactno">
+                                <Form.Label column sm="4">Contact No:</Form.Label>
+                                <Col sm="8">
+                                    <Form.Control type="text" name="contactno" placeholder="Enter Contact Number" onChange={handleChange} value={values.contactno} style={{ borderColor: touched.contactno && errors.contactno ? "red" : null }} />
+                                    {touched.contactno && errors.contactno ? (
+                                        <Col className="error-message">{errors.contactno}</Col>
+                                    ) : null}
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3" controlId="email">
+                                <Form.Label column sm="4">Email:</Form.Label>
+                                <Col sm="8">
+                                    <Form.Control type="text" name="email" placeholder="Enter Email" onChange={handleChange} value={values.email} style={{ borderColor: touched.email && errors.email ? "red" : null }} />
+                                    {touched.email && errors.email ? (
+                                        <Col className="error-message">{errors.email}</Col>
+                                    ) : null}
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row} className="mb-3" controlId="password">
+                                <Form.Label column sm="4">Password:</Form.Label>
+                                <Col sm="8">
+                                    <Form.Control type="password" name="password" placeholder="Enter Password" onChange={handleChange} value={values.password} style={{ borderColor: touched.password && errors.password ? "red" : null }} />
+                                    {touched.password && errors.password ? (
+                                        <Col className="error-message">{errors.password}</Col>
+                                    ) : null}
+                                </Col>
+                            </Form.Group>
+                            <Button variant="outline-primary" size="sm" type="submit" style={{ position: "relative", left: "8rem", marginTop: "10px" }}>SIGN UP</Button>
+                            <Form.Text className="text-muted" style={{ position: "relative", top: "2.5rem" }}>
+                                Already Have Account ? <Link to="/login">SIGN IN</Link>
+                            </Form.Text>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </div>
 
