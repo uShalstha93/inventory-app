1ce36003ac7a43255e5a357fc0b9fbfd3c4c4ba0
Ushal <ushalstha93@gmail.com>
2022-10-26 13:07:36 +0545

    customer update route/api created

--- server/src/routes/CustomerRouter.js
+++ server/src/routes/CustomerRouter.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
 })
 
 //post customer method API
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     try {
         customer.create(req.body)
         res.json({
@@ -35,6 +35,51 @@ router.post('/', async(req, res) => {
         console.log(err)
         res.send({
             errorMsg: "Unable to Post Data in Database!!",
+            errorDetail: err
+        })
+    }
+})
+
+//update customer method API
+router.put('/', async (req, res) => {
+    try {
+        customer.findOneAndUpdate({ customerID: req.body.customerID }, {
+            customerName: req.body.customerName,
+            customerAddress: req.body.customerAddress,
+            customerContactNo: req.body.customerContactNo,
+            customerEmail: req.body.customerEmail
+        })
+            .then(result => {
+                res.json({
+                    message: "Customer Info Updated Successfully!!",
+                    customerDetail: req.body
+                })
+            })
+    }
+    catch (err) {
+        console.log(err)
+        res.send({
+            errorMsg: "Unable to Update Data!!",
+            errorDetail: err
+        })
+    }
+})
+
+//Delete customer method API
+router.delete('/', async (req, res) => {
+    try {
+        customer.deleteOne({ customerID: req.body.customerID })
+        .then(result => {
+            res.json({
+                message: `Customer Deleted Successfully!!`,
+                customerDetail: req.body
+            })
+        })
+    }
+    catch (err) {
+        console.log(err)
+        res.send({
+            errorMsg: "Unable to Delete Customer",
             errorDetail: err
         })
     }
